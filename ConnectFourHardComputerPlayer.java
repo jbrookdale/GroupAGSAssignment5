43c313edd8baf0f321248543ee19cb03cc80945d
damabel <740474@swansea.ac.uk>
2014-03-19 23:04:48 +0000

    just a layout
    nothing much. Still thinking how computer can make an intelligent move

--- ConnectFourHardComputerPlayer.java
+++ ConnectFourHardComputerPlayer.java
@@ -1,68 +1,31 @@
+import java.awt.Point;
 class ConnectFourHardComputerPlayer {
 
-    public int numPieces;
-    public static final int maxPieces = 42, Empty = 2;
+    private int numPieces;
+    private static final int maxPieces = 70, Empty = 2;
     public static boolean[][][] map;
     static int w = 10;
     static int h =7;
     static int n =4;
-    public static final int winPlaces =(4 * w *h) - (3*w*n) - (3*h*n) + (3*w) + (3*h) - (4*n) + (2*n*n);
+    //Equation for checking the winning place on Connect4board
+    //winPlace=(4 * w *h) - (3*w*n) - (3*h*n) + (3*w) + (3*h) - (4*n) + (2*n*n);
+    private static final int winPlaces =143;
     public static void main(String[] args) {
         // Tests go here
-    	System.out.println(winPlaces);
     }
     
     
-
+    // this method checks the state of the board 
       public void Connect4State() {
-      // Initialize the map
-      int i, j, k, count = 0;
-      if (map == null) {
-        map = new boolean[10][7][winPlaces];
-        for (i = 0; i < 10; i++)
-          for (j = 0; j < 7; j++) 
-            for (k = 0; k < winPlaces; k++)
-              map[i][j][k] = false;
+     
       }
-
-        // this loop Set the horizontal win positions
-        for (i = 0; i < 10; i++)
-          for (j = 0; j < 4; j++) {
-            for (k = 0; k < 4; k++)
-              map[j + k][i][count] = true;
-            count++;
-          }
-        
-        // this loop Set the vertical win positions
-        for (i = 0; i < 7; i++){
-          for (j = 0; j < 3; j++) {
-            for (k = 0; k < 4; k++){
-              map[i][j + k][count] = true;
-            count++;
-            }
-          }
-         }
-
-        // Set the forward diagonal win positions 
-        for (i = 0; i < 3; i++){
-          for (j = 0; j < 4; j++) {
-            for (k = 0; k < 4; k++){
-              map[j + k][i + k][count] = true;
-            count++;
-            }
-          }
-        }
-
-        // Set the backward diagonal win positions 
-        for (i = 0; i < 3; i++){
-          for (j = 6; j >= 3; j--) {
-            for (k = 0; k < 4; k++){
-              map[j - k][i + k][count] = true;
-            count++;
-            }
-          }
-        }
-   }
+      public void Move(int x, int y) {
+    	  
+    	}
       
+      // point might not be suitable. may be 2d jlabel array
+      public Point computerMove(int i, int j) {
+		return null;
+      }
 }
       
