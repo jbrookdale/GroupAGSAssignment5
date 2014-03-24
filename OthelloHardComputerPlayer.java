cc02de73f146467e14555597281225bc1382647a
MabelleSnow <mabelsnow520@gmail.com>
2014-03-24 18:26:40 +0000

    save the test part for OthelloHardComputerPlayer
    the problem is the it never goes through the code below just 52 to 63 always .. when running

--- OthelloHardComputerPlayer.java
+++ OthelloHardComputerPlayer.java
@@ -3,9 +3,10 @@ import java.awt.Point;
 import java.util.Random;
 
 class OthelloHardComputerPlayer <C> extends HardComputerPlayer {
+	
+	public int whiteNum = 0;
+	public int blackNum = 0;
     
-
-
 	public OthelloHardComputerPlayer(String newName, Object newPlayerColour) {
 		super(newName, newPlayerColour);
 	}
@@ -16,15 +17,15 @@ class OthelloHardComputerPlayer <C> extends HardComputerPlayer {
         
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                if (getFlipsForPosition(i,j, board) > maximumFlips) {
-                     maximumFlips = getFlipsForPosition(i,j,board);
+                if (getFlipsForPosition(i,j, board.getPieces()) > maximumFlips) {
+                     maximumFlips = getFlipsForPosition(i,j,board.getPieces());
                 }
             }
         }
         
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                if (getFlipsForPosition(i,j, board) == maximumFlips) {
+                if (getFlipsForPosition(i,j, board.getPieces()) == maximumFlips) {
                     bestFlips.add(new Point(i,j));
                 }
             }
@@ -46,14 +47,20 @@ class OthelloHardComputerPlayer <C> extends HardComputerPlayer {
     *
     **/   
     
-    private int getFlipsForPosition(int x, int y, OthelloBoard board) {
-        int flippedPieces = 0;
-                OthelloPiece[][] boardPieces = board.getPieces();
+    private int getFlipsForPosition(int x, int y, OthelloPiece[][] boardPieces) {
+        		
+    		int flippedPieces = 0;
+                OthelloPiece[][] newPieces = boardPieces;
+                
                 //already has piece here
-                if(boardPieces[x][y] != new OthelloPiece(OthelloPiece.OthelloPieceColour.NONE)) {
-                    //System.out.println(boardPieces[x][y]);
-                    return 0;
+                // OthelloPiece temp = new OthelloPiece(Piece.OthelloPieceColour.WHITE);
+                
+                if(!newPieces[x][y].equals(Piece.OthelloPieceColour.NONE)) {
+                    System.out.println(newPieces[x][y]);
+                    return 1;
                 }
+                
+                // it never goes through the code below just 52 to 63 always ..
                         
                     //check pieces
                     for(int i = 0; i < 4; i++)
@@ -61,7 +68,7 @@ class OthelloHardComputerPlayer <C> extends HardComputerPlayer {
                         int off = 1;
                         int tempX = x;
                         int tempY = y;
-                        boolean validPosition = false;
+                        boolean anyValid = false;
                         int end = 1;
                             
                         //two sides of a direction
@@ -117,35 +124,34 @@ class OthelloHardComputerPlayer <C> extends HardComputerPlayer {
                                 }
                                     
                                 //search for the next direction
-                                if(!validPosition && boardPieces[tempX][tempY] == getColour())
+                                if(!anyValid && newPieces[tempX][tempY] == getColour())
                                 {
                                     if(off > 1)
                                     {
                                         j --;
-                                        validPosition = true;
+                                        anyValid = true;
                                         end = off;
                                     }
                                     break;
                                 }
                                 //no piece
-                                if(boardPieces[tempX][tempY] == getColour()) 
+                                if(newPieces[tempX][tempY] == getColour()) 
                                                             break;
                                
                                 //can eat some piece (change colour of them)
-                                if(validPosition)
-                                {
-                                    // There is a white between 2 black pieces.
-                                    if(off < end)	
-                                    {
-                                        flippedPieces++;
-                                }
-                                else
-                                {
-                                    validPosition = false;
-                                    break;
-                                }
-                            }
-                            off ++;
+                                if(anyValid)
+            					{
+            						if(off < end)	
+            						{
+            							flippedPieces ++;
+            						}
+            						else
+            						{
+            							anyValid = false;
+            							break;
+            						}
+            					}
+            					off ++;
                         }
                     }
                 }
@@ -154,8 +160,35 @@ class OthelloHardComputerPlayer <C> extends HardComputerPlayer {
     }
 
 	public static void main(String[] args) {
-        // Tests go here
+		OthelloHardComputerPlayer compAI = new OthelloHardComputerPlayer("CompAI",Piece.OthelloPieceColour.WHITE);
+        OthelloGame game = new OthelloGame(compAI, new HumanPlayer("Mabelle",Piece.OthelloPieceColour.BLACK));
+        
+        for(int j = 0; j < 8; j++){
+        	for(int i = 0; i < 8; i++){
+        		game.board.setLoadedPieces(i, j, new OthelloPiece(Piece.OthelloPieceColour.NONE));
+        	}
+        }
+        
+        game.board.setLoadedPieces(3, 4, new OthelloPiece(Piece.OthelloPieceColour.BLACK));
+        game.board.setLoadedPieces(3, 3, new OthelloPiece(Piece.OthelloPieceColour.WHITE));
+        game.board.setLoadedPieces(4, 4, new OthelloPiece(Piece.OthelloPieceColour.WHITE));
+        game.board.setLoadedPieces(4, 3, new OthelloPiece(Piece.OthelloPieceColour.BLACK));
+        
+        
+        OthelloPiece[][] test = game.board.getPieces();
+        System.out.println(test[3][4].getPieceColour());
+        
+        
+        int x = compAI.getFlipsForPosition(5,3, test);
+        System.out.println(x);
+        
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+            	int y = compAI.getFlipsForPosition(i,j, test);
+                System.out.println(y);
+            }
+        }
     }
-	
+
 
 }
