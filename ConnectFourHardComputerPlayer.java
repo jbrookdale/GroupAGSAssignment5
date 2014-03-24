b8e94651f805cff83dfade26ef3c7c41f1f557b4
damabel <740474@swansea.ac.uk>
2014-03-24 23:02:00 +0000

    update on connect4hard
    a computer move that extends the  longest chain and randomly pick one if
    many of equal length exist. Still thinking of how to get the longest
    chain on board

--- ConnectFourHardComputerPlayer.java
+++ ConnectFourHardComputerPlayer.java
@@ -1,16 +1,16 @@
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Random;
 
 class ConnectFourHardComputerPlayer <C> extends HardComputerPlayer {
 
-    private int numPieces;
-    private static final int maxPieces = 70, Empty = 2;
-    public static boolean[][][] map;
-    static int w = 10;
-    static int h = 7;
+    
+    private static int Height = 10;
+    private static int Width = 7;
     static int n = 4;
     //Equation for checking the winning place on Connect4board
     //winPlace=(4 * w *h) - (3*w*n) - (3*h*n) + (3*w) + (3*h) - (4*n) + (2*n*n);
-    private static final int winPlaces =143;
+   // private static final int winPlaces =143;
     public ConnectFourHardComputerPlayer(String newName, C newPlayerColour) {
  		super(newName, newPlayerColour);
  	}
@@ -22,36 +22,42 @@ class ConnectFourHardComputerPlayer <C> extends HardComputerPlayer {
     }
     
     
-    // this method checks the state of the board and calls computermove method
-    //Just an idea ...sort of pseodo
-    //Find a winning computer move first, or if that fails, find a move that blocks the human;
-    //if that fails, too, then just choose a random move.
-    //Look for vertical runs of 2 or 3 length;  if they are the human player's(opponent) vertical runs stop it, 
-    //or continue them if they are the computer player's vertical runs
-    //same for horizontal and diagonal.
-      public void Connect4State() {
-    	 /* if(totalpiece on board <= 6) { 
-    			drop piece on any of my longest chain if it is a valid move
-    		  }
+   
+	//The move made should extend the longest chain(horizontal, vertical or diagonal) 
+	//on the board by one
+	// randomly pick one if such equal chain exist
+    	  public Point makeAIMove(ConnectFourBoard board) {
+    	        int maximum = 0;
+    	        ArrayList<Point> max_choice = new ArrayList<Point>();
+    	        
+    	        for (int i = 0; i < Height; i++) {
+    	            for (int j = 0; j < Width; j++) {
+    	                if (MaximumChain(i,j, board) > maximum) {
+    	                     maximum = MaximumChain(i,j, board);
+    	                }
+    	            }
+    	        }
+    	        
+    	        for (int i = 0; i < Height; i++) {
+    	            for (int j = 0; j < Width; j++) {
+    	            	// checking if more than one maximum chain exist
+    	                if (MaximumChain(i,j, board) == maximum) {
+    	                    max_choice.add(new Point(i,7));
+    	                }
+    	            }
+    	        }
+    	        
+    	        Random r = new Random();
+    	        int arrayPosition = r.nextInt(max_choice.size());
+    	        return max_choice.get(arrayPosition);
+    	    }
+      // This method should return the longest chain
+    // We need to return just the column to pace the piece after getting the maximum chain 
+      private int MaximumChain(int i, int j, ConnectFourBoard board) {
+			
+			return 1; 
+		}
 
-    		}else if(my longest chain >= longest chain of opponent) {
-    		  if(my longest chain piece== 3 && my move is a winning move{ make move}
-    		  else{add piece to any of the longest chain of opponent}
-    		  
-    		 else if(there are valid moves) {drop piece on my previous move if it is valid}
-    		 else if(there are valid moves) {drop piece beside any of my piece if it is a valid move)
-    		 else(make a random move)
-    		 */
-     
-      }
-      public void Move(int x, int y) {
-    	  
-    	  
-    	  
-    	}
-      
-      public Point computerMove(int i, int j) {
-		return null;
-      }
+
 }
       
