498fe30269fc96629ea1fdc82dbc9fcfe0da73b5
damabel <740474@swansea.ac.uk>
2014-03-25 15:18:59 +0000

    update
    on connect4hard

--- ConnectFourHardComputerPlayer.java
+++ ConnectFourHardComputerPlayer.java
@@ -7,10 +7,6 @@ class ConnectFourHardComputerPlayer <C> extends HardComputerPlayer {
     
     private static int Height = 10;
     private static int Width = 7;
-    static int n = 4;
-    //Equation for checking the winning place on Connect4board
-    //winPlace=(4 * w *h) - (3*w*n) - (3*h*n) + (3*w) + (3*h) - (4*n) + (2*n*n);
-   // private static final int winPlaces =143;
     public ConnectFourHardComputerPlayer(String newName, C newPlayerColour) {
  		super(newName, newPlayerColour);
  	}
@@ -23,33 +19,29 @@ class ConnectFourHardComputerPlayer <C> extends HardComputerPlayer {
     
     
    
-	//The move made should extend the longest chain(horizontal, vertical or diagonal) 
-	//on the board by one
-	// randomly pick one if such equal chain exist
+	//Scan the board for position that has maximum chain length
+	// randomly pick one if there are more than one chain length
     	  public Point makeAIMove(ConnectFourBoard board) {
+    		  Random r = new Random();
     	        int maximum = 0;
     	        ArrayList<Point> max_choice = new ArrayList<Point>();
+    	        int arrayPosition;
     	        
     	        for (int i = 0; i < Height; i++) {
     	                if (MaximumChain(i,board.getLowestEmptySlot(i), board) > maximum) {
     	                     maximum = MaximumChain(i,board.getLowestEmptySlot(i), board);
-    	                
-    	            }
+    	                 }
     	        }
-    	        
     	        for (int i = 0; i < Height; i++) {
     	            	// checking if more than one maximum chain exist
     	                if (MaximumChain(i,board.getLowestEmptySlot(i), board) == maximum) {
-    	                    max_choice.add(new Point(i,7));
+    	                    max_choice.add(new Point(i,Width));
     	                }
     	        }
-    	        
-    	        Random r = new Random();
-    	        int arrayPosition = r.nextInt(max_choice.size());
+    	         arrayPosition= r.nextInt(max_choice.size());
     	        return max_choice.get(arrayPosition);
     	    }
-      // This method should return the longest chain
-    // We need to return just the column to pace the piece after getting the maximum chain 
+      // This method return the longest chain length.
       private int MaximumChain(int x, int y, ConnectFourBoard board) {
       int chainLength = 0;
       Piece pieces[][] = board.getPieces();
@@ -70,7 +62,7 @@ class ConnectFourHardComputerPlayer <C> extends HardComputerPlayer {
 
                     // If the piece which stops the loop above is the players own colour, add the counter
                     // To the total number of flippied pieces, else don't add anything.
-                    if ((x+(i*counter)) >= 0 && (x+(i*counter)) < 8 && (y+(j*counter)) >= 0 && (y+(j*counter)) < 8) {
+                    if ((x+(i*counter)) >= 0 && (x+(i*counter)) < Height && (y+(j*counter)) >= 0 && (y+(j*counter)) < Width) {
                         if (!((x+(i*counter)) == x && (y+(j*counter)) == y)) {
                             
                                 if ((counter - 1) > chainLength)
