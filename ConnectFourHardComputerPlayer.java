457d0ef8eb605a08ef0cca82ad63398f1f3de531
MabelleSnow <mabelsnow520@gmail.com>
2014-03-26 13:34:46 +0000

    changing names and fix magic number

--- ConnectFourHardComputerPlayer.java
+++ ConnectFourHardComputerPlayer.java
@@ -7,11 +7,15 @@ class ConnectFourHardComputerPlayer <C> extends HardComputerPlayer {
 
     private static int HEIGHT = 7;
     private static int WIDTH = 10;
+    private final static int NEGATIVE_X_AXIS = -1;
+	private final static int POSITIVE_X_AXIS = 1;
+	private final static int NEGATIVE_Y_AXIS = -1;
+	private final static int POSITIVE_Y_AXIS = 1;
+	
+    
     public ConnectFourHardComputerPlayer(String newName, C newPlayerColour) {
  		super(newName, newPlayerColour);
  	}
-
-
 
 	public static void main(String[] args) {
         // Tests go here
@@ -23,58 +27,56 @@ class ConnectFourHardComputerPlayer <C> extends HardComputerPlayer {
 	// randomly pick one if there are more than one chain length
     	  public Point makeAIMove(ConnectFourBoard board) {
     		  Random r = new Random();
-    	        int maximum = 0;
-    	        ArrayList<Point> max_choice = new ArrayList<Point>();
-    	        int arrayPosition;
+    	      int maximum = 0;
+    	      ArrayList<Point> m_MaxChoice = new ArrayList<Point>();
+    	      int arrayPosition;
     	        
-    	        for (int i = 0; i < WIDTH; i++) {
-    	                if (MaximumChain(i,board.getLowestEmptySlot(i), board) > maximum) {
-    	                     maximum = MaximumChain(i,board.getLowestEmptySlot(i), board);
-    	                 }
-    	        }
+    	      for (int i = 0; i < WIDTH; i++) {
+    	           if (MaximumChain(i,board.getLowestEmptySlot(i), board) > maximum) {
+                   maximum = MaximumChain(i,board.getLowestEmptySlot(i), board);
+  	               }
+    	      }
     
-                
-    
-    	        for (int i = 0; i < WIDTH; i++) {
+            
+    	      for (int i = 0; i < WIDTH; i++) {
     	            	// checking if more than one maximum chain exist
-    	                if (MaximumChain(i,board.getLowestEmptySlot(i), board) == maximum) {
-    	                    max_choice.add(new Point(i,HEIGHT));
-    	                }
-    	        }
+                   if (MaximumChain(i,board.getLowestEmptySlot(i), board) == maximum) {
+                   m_MaxChoice.add(new Point(i,HEIGHT));
+    	           }
+    	      }
     
-    	        arrayPosition= r.nextInt(max_choice.size());
-    	        return max_choice.get(arrayPosition);
-    	    }
+    	      arrayPosition= r.nextInt(m_MaxChoice.size());
+    	      return m_MaxChoice.get(arrayPosition);
+    	  }
+    	  
       // This method return the longest chain length.
       private int MaximumChain(int x, int y, ConnectFourBoard board) {
-          int chainLength = 0;
-          Piece pieces[][] = board.getPieces();
+          int m_chainLength = 0;
+          Piece m_Pieces[][] = board.getPieces();
         // If there is not a piece at position (x,y) then work out long a chain it connects to
-        if (pieces[x][y].getPieceColour() == ConnectFourPiece.ConnectFourPieceColour.NONE) {
-            for (int i = -1; i <= 1; i++) { // These signify directions on a graph in the x-axis
-                for (int j = -1; j <= 1; j++) { // These signify directions on a graph in the y-axis
+        if (m_Pieces[x][y].getPieceColour() == ConnectFourPiece.ConnectFourPieceColour.NONE) {
+        	 for (int i = NEGATIVE_X_AXIS; i <= POSITIVE_X_AXIS; i++) { // These signify directions on a graph in the x-axis
+                 for (int j = NEGATIVE_Y_AXIS; j <= POSITIVE_Y_AXIS; j++) { // These signify directions on a graph in the y-axis
                     int counter = 1;
                     
                     if ((x+(i*counter)) >= 0 && (x+(i*counter)) < WIDTH && (y+(j*counter)) >= 0 && (y+(j*counter)) < HEIGHT) {
                         // Loop while pieces are the players colour. Add 1 to counter for each iteration.
                         while (((x+(i*counter)) >= 0) && ((x+(i*counter)) < WIDTH) && ((y+(j*counter)) >= 0) && ((y+(j*counter)) < HEIGHT)
-                                && pieces[x+(i*counter)][y+(j*counter)].getPieceColour() == getColour()) {
+                                && m_Pieces[x+(i*counter)][y+(j*counter)].getPieceColour() == getColour()) {
                             counter++;
                         }
                     }
 
                         if (!((x+(i*counter)) == x && (y+(j*counter)) == y)) {
-                                if ((counter - 1) > chainLength) {
-                                    chainLength = (counter - 1);
+                                if ((counter - 1) > m_chainLength) {
+                                	m_chainLength = (counter - 1);
                                 }
                         }
                 }
             }
         }
         
-        return chainLength;
-		}
-
-
+        return m_chainLength;
+        }
 }
       
