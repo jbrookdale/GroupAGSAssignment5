28254e1bcd45966c2c358840d967046300702a22
damabel <740474@swansea.ac.uk>
2014-03-26 16:23:51 +0000

    dox on player class
    player class dox

--- Player.java
+++ Player.java
@@ -22,36 +22,72 @@ import java.awt.Point;
 
 
 abstract class Player <C> {
+	/** Variable for the Player name */
     private String m_Name;
+    /** Variable for the Player colour */
     private C m_PlayerColour;
+    /** Variable that stores player's score */
     private int m_Score = 0;
+    /** Variable for the type of Player */
     private String m_PlayerType;
     
+    /**
+     * This method gets the name of player
+     * @return -return player name
+     */
     public String getName () {
         return m_Name;
     }
     
+    /**
+     * This method gets the score of player
+     * @return -return player's score
+     */
     public int getScore() {
         return m_Score;
     }
-
+    
+    /**
+     * This method gets the colour of player's piece
+     * @return m_PlayerColour -return player's colour
+     */
     public C getColour () {
         return m_PlayerColour;
     }
+    
+    /**
+     * This method sets of player's score
+     */
     public void setScore(int points) {
         m_Score+=points;
     }
-
+    
+    /**
+     * This method gets the  player's type
+     * @return m_PlayerType -return player type
+     */
+    public String getPlayerType() {
+        return m_PlayerType;
+    }
+   /**
+    * This is the constructor for Player class.
+    * It initialise the player name, player colour and player typr
+    * @param newName - Name of player
+    * @param newPlayerColour -Colour of player piece
+    * @param playerType - Type of player
+    */
     public Player (String newName, C newPlayerColour, String playerType){
         m_Name = newName;
         m_PlayerColour = newPlayerColour;
         m_PlayerType = playerType;
     }
     
-    public String getPlayerType() {
-        return m_PlayerType;
-    }
-    	
+    /**
+     * This method checks if move method is executed
+     * @param x - Horizontal coordinate
+     * @param y -vertical coordinate
+     * @return false -Test if method execute
+     */
      boolean move (int x, int y) {
 		return false;
 	}
