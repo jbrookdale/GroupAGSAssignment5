9afd45abd81ec50a00058693dcf0a5146e645da5
damabel <740474@swansea.ac.uk>
2014-03-26 15:51:38 +0000

    dox
    dox on easy computer player

--- EasyComputerPlayer.java
+++ EasyComputerPlayer.java
@@ -1,12 +1,32 @@
 import java.awt.Point;
+/**
+* @file EasyComputerPlayer.java
+* @author Jon Bailey
+* @date  21st Mar 2014
+* @see 'JAVA for everyone' - Cay Horstmann, Second edition, for Abstract class
+*
+* @brief This is an Abstract EasyComputerPlayer class. 
+*		
+* This class calls the name, piece colour of player and pass in ComputerEasy player
+*
+*/
 
 abstract class EasyComputerPlayer <C> extends ComputerPlayer {
-    
+    /**
+     * EasyComputerPlaye Constructor
+     * @param newName -The name of the player
+     * @param newPlayerColour - The piece colour of the player
+     */
     public EasyComputerPlayer(String newName, C newPlayerColour) {
 		super(newName, newPlayerColour,"ComputerEasy");
 		
 	}
-
+ /**
+  * This is the main method that runs all the required test
+  * @param args - nil
+  */
+ 
+ 
 	public static void main(String[] args) {
         // Tests go here
     }
